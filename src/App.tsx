0633d4fb3c02c8ac5cddca13b4da4173ebb1b152
stapler-codes <jadkoneissi@gmail.com>
2024-03-29 14:23:26 +0200

    redesign

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,12 @@
 import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
-import pic1 from "./assets/pic1.webp";
-import pic2 from "./assets/pic2.webp";
-import pic3 from "./assets/pic3.webp";
-import pic4 from "./assets/pic4.webp";
-import pic5 from "./assets/pic5.webp";
-import pic6 from "./assets/pic6.webp";
-import pic7 from "./assets/pic7.webp";
-import pic8 from "./assets/pic8.webp";
-import pic9 from "./assets/pic9.webp";
-import pic10 from "./assets/pic10.webp";
-import pic11 from "./assets/pic11.webp";
-import pic12 from "./assets/pic12.webp";
-import pic13 from "./assets/pic13.webp";
-import pic14 from "./assets/pic14.webp";
+import { products } from "./Products";
 import "./index.css";
 import React, { useEffect, useState } from "react";
 import { v4 } from "uuid";
 import axios from "axios";
 import { Analytics } from "@vercel/analytics/react";
+import type { Product } from "./types";
 
-interface Product {
-  name: string;
-  price5ml: number;
-  price10ml: number;
-  quantity: number;
-  id: number;
-  imgPath?: string;
-  uid?: string;
-  finalPrice: number;
-}
 export default function App() {
   const [cart, setCart] = useState<Product[]>([]);
   const [total, setTotal] = useState<number>(0);
@@ -51,134 +29,6 @@ export default function App() {
     }
   }, []);
 
-  const products: Product[] = [
-    {
-      name: "Azuree Estee Lauder (for women)",
-      price5ml: 15,
-      price10ml: 26,
-      quantity: 1,
-      id: 1,
-      imgPath: pic1,
-      finalPrice: 0,
-    },
-    {
-      name: "Good Girl Supreme Carolina Herrera (for women)",
-      price5ml: 15,
-      price10ml: 25,
-      quantity: 2,
-      id: 2,
-      imgPath: pic2,
-      finalPrice: 0,
-    },
-    {
-      name: "The Only One Dolce&Gabbana (for women)",
-      price5ml: 15,
-      price10ml: 23,
-      quantity: 3,
-      id: 3,
-      imgPath: pic3,
-      finalPrice: 0,
-    },
-    {
-      name: "Gucci Bloom Nettare Di Fiori Gucci (for women)",
-      price5ml: 17,
-      price10ml: 26,
-      quantity: 4,
-      id: 4,
-      imgPath: pic4,
-      finalPrice: 0,
-    },
-    {
-      name: "Le Male Jean Paul Gaultier (for men)",
-      price5ml: 13,
-      price10ml: 25,
-      quantity: 5,
-      id: 5,
-      imgPath: pic5,
-      finalPrice: 0,
-    },
-    {
-      name: "Le Male Le Parfum Jean Paul Gaultier (for men)",
-      price5ml: 13,
-      price10ml: 25,
-      quantity: 6,
-      id: 6,
-      imgPath: pic6,
-      finalPrice: 0,
-    },
-    {
-      name: "9pm Afnan (for men)",
-      price5ml: 5,
-      price10ml: 10,
-      quantity: 7,
-      id: 7,
-      imgPath: pic7,
-      finalPrice: 0,
-    },
-    {
-      name: "Coco Mademoiselle Intense Chanel (for women)",
-      price5ml: 20,
-      price10ml: 30,
-      quantity: 8,
-      id: 8,
-      imgPath: pic8,
-      finalPrice: 0,
-    },
-    {
-      name: "Sauvage Eau de Parfum Dior (for men)",
-      price5ml: 15,
-      price10ml: 25,
-      quantity: 9,
-      id: 9,
-      imgPath: pic9,
-      finalPrice: 0,
-    },
-    {
-      name: "Gentleman Only Givenchy (for men)",
-      price5ml: 17,
-      price10ml: 28,
-      quantity: 10,
-      id: 10,
-      imgPath: pic10,
-      finalPrice: 0,
-    },
-    {
-      name: "Versace Crystal Noir Eau De Parfum",
-      price5ml: 16,
-      price10ml: 25,
-      quantity: 10,
-      id: 11,
-      imgPath: pic11,
-      finalPrice: 0,
-    },
-    {
-      name: "Aventus Creed (for men)",
-      price5ml: 23,
-      price10ml: 35,
-      quantity: 10,
-      id: 12,
-      imgPath: pic12,
-      finalPrice: 0,
-    },
-    {
-      name: "Valentino Uomo Born in Roma Valentino (for men)",
-      price5ml: 15,
-      price10ml: 25,
-      quantity: 10,
-      id: 13,
-      imgPath: pic13,
-      finalPrice: 0,
-    },
-    {
-      name: "Toy Boy Moschino for men (coming soon)",
-      price5ml: 13,
-      price10ml: 20,
-      quantity: 10,
-      id: 14,
-      imgPath: pic14,
-      finalPrice: 0,
-    },
-  ];
   return (
     <BrowserRouter>
       <Analytics />
@@ -222,9 +72,15 @@ export default function App() {
 function Nav() {
   return (
     <nav className="">
-      <Link to={"/"}>Home</Link>
-      <Link to={"/cart"}>Cart</Link>
-      <Link to={"/checkout"}>Checkout</Link>
+      <Link to={"/"} id="navLink">
+        Home
+      </Link>
+      <Link to={"/cart"} id="navLink">
+        Cart
+      </Link>
+      <Link to={"/checkout"} id="navLink">
+        Checkout
+      </Link>
     </nav>
   );
 }
---
+++ src/types.ts
@@ -0,0 +1,10 @@
+export interface Product {
+  name: string;
+  price5ml: number;
+  price10ml: number;
+  quantity: number;
+  id: number;
+  imgPath?: string;
+  uid?: string;
+  finalPrice: number;
+}
